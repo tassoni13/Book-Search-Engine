eac04478b35c70fc0d2b093a502eb98986b0cfc7
tassoni13 <tassoni13@gmail.com>
2021-09-18 02:42:57 -0400

    update

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,19 +4,19 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        me: async (parent, args, context) => {
+        me: async ({parent, args, context}) => {
             if (context.user) {
                 return User.findOne({ _id: context.user._id }).populate('savedBooks');
             }
             throw new AuthenticationError('You need to be logged in!')
         },
     Mutation: {
-        addUser: async (parent, { username, email, password }) => {
+        addUser: async ({ parent }, { username, email, password }) => {
             const user = await User.create({ username, email, password });
             const token = signToken(user);
             return { token, user };
         },
-        login: async (parent, { email, password }) => {
+        login: async ({ parent }, { email, password }) => {
             const user = await User.findOne({ email });
 
             if (!user) {
